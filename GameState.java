ddeebccd032d59a1cb3809fc1637bba3733feed6
Alex Pieczynski <apiecz2@uic.edu>
2016-04-18 14:23:25 -0500

    Added GameState class with some methods 	-keeps track of the score, level, etc 	-contains Timer that will lower a Tetromino each time it fires 	-may need a reference to the currently falling tetromino in order 	 drop it when the timer fires

---
+++ GameState.java
@@ -0,0 +1,63 @@
+//class used to represent the current state of the Tetris game
+import java.util.Timer;
+
+public class GameState
+{
+  private int score;
+  private int linesCleared;
+  private int level;
+  private Tetromino currentPiece; //reference to currently falling piece?
+  private PieceType nextPiece;
+  private Timer timer;
+  
+  public GameState()
+  {
+    score = 0;
+    linesCleared = 0;
+    level = 0;
+    nextPiece = PieceType.NULL; //use PieceFactroy class
+    //timer = new Timer(1000, new TimeHandler());
+    //timer.start();
+  }
+  
+  public void pieceDropped()
+  {
+    score += 10;
+    nextPiece = PieceType.NULL; //use PieceFactory class
+  }
+  
+  
+  public void linesCleared(int n)
+  {
+    linesCleared += n;
+    
+    //update score based on n
+    switch (n)
+    {
+      case 1:
+        score += 40*level;
+        break;
+      case 2:
+        score += 100*level;
+        break;
+      case 3:
+        score += 300*level;
+        break;
+      case 4:
+        score += 1200*level;
+        break;
+      default: //error, called with invalid n
+        score = -1;
+        break;
+    }
+  }
+  
+  
+  //function that gets called when the Timer fires
+  private class TimeHandler implements ActionListener{
+    public void actionPerformed( ActionEvent event )
+    {
+      //make piece drop
+    }
+  }
+}
